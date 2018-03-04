35a75874d810be78b36801b795778a9bdbf2c7bc
jpenrici <jpenrici@gmail.com>
2018-03-04 17:38:58 -0300

    Mudança nos limites Max e Min.
    Cada valor no vetor, terá um Max e Min individuais.

--- Cpp/counter_using_vector.cpp
+++ Cpp/counter_using_vector.cpp
@@ -25,10 +25,12 @@ using std::stringstream;
 
 class Counter {
 public:
-    Counter(int num, int min, int max, int value): min(min), max(max) {
+    Counter(int num, int min, int max, int value) {
         if (value < min) value = min;
         if (value > max) value = max;
         counter.resize(num, value);
+        this->min.resize(num, min);
+        this->max.resize(num, max);
         vSize = counter.size() - 1;
     };
     ~Counter(){};
@@ -36,11 +38,15 @@ public:
     void add() { calc( counter, vSize,  1); }
     void sub() { calc( counter, vSize, -1); }
     bool calc(vector<int>& v, int pos, int value);
+    bool setMax(int pos, int newMax);
+    bool setMin(int pos, int newMin);
     string view();
 
 private:
-    int min, max, vSize;
+    int vSize;
     vector<int> counter;
+    vector<int> min;
+    vector<int> max;
     string int2str (int value);
 };
 
@@ -51,6 +57,24 @@ string Counter::int2str (int value)
     return strInt.str();
 }
 
+bool Counter::setMax (int pos, int newMax)
+{
+    if (max.empty()) return false;
+    if (pos < 0 && pos > vSize) return false;
+    if (newMax < min[pos]) return false;
+    max[pos] = newMax;
+    return true;
+}
+
+bool Counter::setMin (int pos, int newMin)
+{
+    if (min.empty()) return false;
+    if (pos < 0 && pos > vSize) return false;
+    if (newMin > max[pos]) return false;
+    min[pos] = newMin;
+    return true;
+}
+
 bool Counter::calc(vector<int>& v, int pos, int value)
 {
     if (value == 0) return false;
@@ -59,12 +83,12 @@ bool Counter::calc(vector<int>& v, int pos, int value)
 
     v[pos] = v[pos] + value;
 
-    if (v[pos] > max) {
-        v[pos] = min;
+    if (v[pos] > max[pos]) {
+        v[pos] = min[pos];
         if (pos > 0) calc(v, pos -1, value);
     } else {
-        if (v[pos] < min) {
-            v[pos] = max;
+        if (v[pos] < min[pos]) {
+            v[pos] = max[pos];
             if (pos > 0) calc(v, pos -1, value);
         }
     }
@@ -83,16 +107,18 @@ string Counter::view()
 int main()
 {
     int i;
-    Counter a (4, 0, 2, 0);
+    Counter a (4, 0, 1, 0);
     cout << a.view() << " *\n";
 
-    for (i = 1; i <= 80; ++i) {
+    a.setMax(0, 5);
+
+    for (i = 1; i <= 40; ++i) {
         a.add();
         cout << a.view() << '\n';
     }
     cout << '\n' << a.view() << " *\n";
 
-    for (i = 1; i <= 82; ++i) {
+    for (i = 1; i <= 10; ++i) {
         a.sub();
         cout << a.view() << '\n';
     }
