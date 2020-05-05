dde62bfe7958289e3da3e756da24b9178efb50f5
jpenrici <jpenrici@gmail.com>
2020-05-05 20:49:00 -0300

    Listando arquivos e diretórios de Miscellaneous. 	new file:   Python/Listar_Conteudo_Diretorio/contents.py 	new file:   Python/Listar_Conteudo_Diretorio/contents_miscellaneous.sh 	new file:   directory_contents.txt

---
+++ Python/Listar_Conteudo_Diretorio/contents_miscellaneous.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# diretório local
+local="$PWD/contents.py"
+saida="directory_contents.txt"
+
+# subindo nível
+cd ../..
+ls
+
+# python3 contents.py type="tipo" path="caminho" header="False/True"
+python3 $local path="Algorithms" header=False > $saida
+python3 $local path="Cpp" header=False >> $saida
+python3 $local path="OpenBox" header=False  >> $saida
+python3 $local path="Python" header=False >> $saida
+python3 $local path="Sublime_Text" header=False >> $saida
+
+echo ""
+cat $saida
