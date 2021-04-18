03de072ee81bfead192de5aba75e51734ff0d475
Juan Pablo Enrici <jpenrici@gmail.com>
2021-04-18 16:32:26 -0300

    Add files via upload

--- Sublime_Text/Create_Sublime-Snippet_Cpp/create_snippets_cpp_header.py
+++ Sublime_Text/Create_Sublime-Snippet_Cpp/create_snippets_cpp_header.py
@@ -18,8 +18,7 @@ SPACE = chr(32)
 MODEL = """<!-- Automatically built. Update as needed! -->
 <snippet>
     <description>#FUNCTION#(#PARAMS#)</description>
-    <content><![CDATA[/* #COMMENT# */
-#FUNCTION#(#CODE#)]]></content>
+    <content><![CDATA[#COMMENT##FUNCTION##CODE#]]></content>
     <tabTrigger>#FUNCTION#</tabTrigger>
     <scope>source.c++</scope>
 </snippet> 
@@ -32,6 +31,7 @@ def load(path):
         f = open(path)
         lines = [line for line in f]
         f.close()
+        print("Loaded: {}".format(path))
     except Exception:
         print("error: couldn't open file.")
         pass
@@ -41,20 +41,32 @@ def load(path):
 
 def save(path, text):
     try:
-        path = open(path, "w")
-        path.write(text)
-        path.close()
+        f = open(path, "w")
+        f.write(text)
+        f.close()
+        print("Saved: {}".format(path))
     except Exception:
         print("error: cannot save " + path)
         exit(0)
 
 
 def prepare(data):
-
+    #
+    # Exclusive function for preparing C++ header files.
+    #
     if len(data) == 0:
         print("Empty")
         return
 
+    # Step 1:
+    # Separate lines with words in parentheses.
+    #
+    # Example:
+    #          Words () { ... }
+    #          Words ( words )
+    #          Words (
+    #                 words ...
+    #          )
     text = ""
     for i in range(len(data)):
         if LEFTP in data[i]:
@@ -69,9 +81,12 @@ def prepare(data):
                 text += data[j].replace(EOL, EMPTY).strip()
             i = j
 
+    # Step 2:
+    # Separate potential constructors and functions.
     data = []
     for i in text.split(END):
         if len(i) > 0:
+            # Exclude destructors.
             if i[0] == '~':
                 continue
             # Remove internal spaces, comments and blocks ...
@@ -80,40 +95,58 @@ def prepare(data):
                     i = i.replace(s, SPACE)
         data += [i]
 
-    # data = list(set(data))
-    # print(data)
+    # Step 3:
+    # Organize ... 
+    data = list(set(data))
 
+    # Step 4:
+    # Analyze and prepare output.
     snippets = []
     for i in data:
         if len(i) > 0:
             # i = 'Type Function ( Type Param, ... )'
-            comment = i
+            comment = "/* " + i + " */" + EOL
 
             # i = [ 'Type Function', 'Type Param, ...' ]
             i = i.replace(RIGHTP, EMPTY).split(LEFTP)
-            func = i[0].split(SPACE)[-1]    # Function
+            func = i[0].split(SPACE)[-1]    # 'Function'
 
-            params, code = EMPTY, EMPTY
-            p = i[-1].split(COMMA)          # [ 'Type Param,', '...' ]
+            code = LEFTP
+            params = EMPTY
+            p = i[-1].split(COMMA)          # [ 'Type Param', '...' ]
             for j in range(len(p)):
                 p[j] = p[j].strip()
-                if p[j].replace(SPACE, EMPTY) != EMPTY:
-                    name = p[j].split(SPACE)[-1]    # [ 'Type', 'Param']
+                if p[j].replace(SPACE, EMPTY) == EMPTY:
+                    # Format: Word ()
+                    code = "${0:/* Code or () */}"
+                    params = EMPTY
+                    comment = EMPTY
+                else:
+                    # Format: Word (word, word, ...)
+                    name = p[j].split(SPACE)[-1]    # [ 'Type', 'Param' ]
                     params += name
                     params += COMMA if j < len(p) - 1 else EMPTY
-                    code += "${" + str(j) + ":/* "
+                    code += "${" + str(j + 1) + ":/* "
                     code += name + " */}" if len(name) > 0 else "Code */}"
-                    code += COMMA if j < len(p) - 1 else EMPTY          
-                
+                    code += COMMA if j < len(p) - 1 else RIGHTP          
+
+            # Model
             result = MODEL.replace("#FUNCTION#", func.strip(SPACE))
             result = result.replace("#PARAMS#", params.strip(SPACE))
             result = result.replace("#COMMENT#", comment.strip(SPACE))
             result = result.replace("#CODE#", code.strip(SPACE))
-            snippets += [result]
+
+            # Output
+            snippets += [
+                {'name': func, 'function': comment, 'snippet': result}
+            ]
 
     return snippets
 
-def main():
+
+def test():
+
+    print("Script initialized.")
 
     # Load C++ file with classes and functions
     data = load("test.h")
@@ -122,10 +155,22 @@ def main():
     snippets = prepare(data)
 
     # Save
+    csv = "Name;Path;Function;Number" + EOL
     for i in range(len(snippets)):
-        path = "./Output/SCpp_" + str(i) + ".sublime-snippet"
-        save(path, snippets[i])
+        name = snippets[i]['name']
+        func = snippets[i]['function']
+        snippet = snippets[i]['snippet']
+        path = "./Output/SCpp{:02d}_{}.sublime-snippet".format(i, name)
+        
+        csv += "{};{};{};{} {}".format(name, func, path, i, EOL)
+        save(path, snippet)
+        # print(snippet)
+
+    save("output_snippets.csv", csv)
+    # print(csv)
+
+    print("Script finished.")
 
 
 if __name__ == '__main__':
-    main()
+    test()
