bc76b5d9569915b932c9aac7c13ac04dedc08457
Juan Pablo Enrici <jpenrici@gmail.com>
2023-01-13 16:10:02 -0300

    Update.

--- Cpp/Support_Scripts/hcpp_generator2.sh
+++ Cpp/Support_Scripts/hcpp_generator2.sh
@@ -2,22 +2,25 @@
 #
 # H and Cpp generator from template.
 #
-# use: ./hcpp_generator2.sh filename1 filename2 ... filenameN
+# use: ./hcpp_generator2.sh dir=destination filename1 filename2 ... filenameN
+#
+#       Attention: the destination path syntax will not be checked! 
 #
 # result:
 #           filename.h          filename.cpp
 #
 #           # pragma once       #include "filename.h"
 #
-#           // code             // code
+#           // model code       // model code
 #
 
-filenames="$@"
+dir="."
+args="$@"
 models=("model.h" "model.cpp")
 
 echo "H Cpp - Generator 2"
 
-if [[ "$filenames" = "" ]]; then
+if [[ "$args" = "" ]]; then
     echo "Empty argument!"
     echo "Finished!"
     exit 0
@@ -31,20 +34,35 @@ for model in ${models[@]}; do
     fi
 done
 
-for filenames
-do
-    hfile="$filenames.h"
-    cfile="$filenames.cpp"
-    str=${filenames^}
+filenames=($args)
+str="${filenames[0]}"
+if [[ $str = "dir="* ]]; then
+    dir=${str:4}
+    filenames=("${filenames[@]:1}")
+    if [[ ! -d $dir ]]; then
+        echo "Create: $dir"
+        mkdir -p $dir
+    fi   
+fi
 
-    echo "$filenames: $hfile $cfile"
+echo "Directory: $dir"
+
+for filename in ${filenames[@]}
+do
+    hfile="$filename.h"
+    cfile="$filename.cpp"
+    str=${filename^}
+
+    echo "$filename: $hfile $cfile"
+
+    # copy model
+    cat model.h   > "$dir/$hfile"
+    cat model.cpp > "$dir/$cfile"
     
-    cat model.h > $hfile
-    cat model.cpp > $cfile
-    
-    sed -i "s/ModelClass/$str/g" $hfile
-    sed -i "s/ModelHeader.h/$hfile/g" $cfile
-    sed -i "s/ModelClass/$str/g" $cfile
+    # replace important snippets
+    sed -i "s/ModelClass/$str/g"      "$dir/$hfile"
+    sed -i "s/ModelHeader.h/$hfile/g" "$dir/$cfile"
+    sed -i "s/ModelClass/$str/g"      "$dir/$cfile"
 done
 
 echo "Finished!"
