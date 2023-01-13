009e1e69dbdf4b29f6acb7ed6be613002be24569
Juan Pablo Enrici <jpenrici@gmail.com>
2023-01-13 10:08:18 -0300

    Simple script to generate Header and C++ files.

---
+++ Cpp/Support_Scripts/hcpp_generator.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# H and Cpp file generator.
+#
+# use: ./hcpp_generator.sh filename1 filename2 ... filenameN
+#
+# result:
+#			filename.h 			filename.cpp
+#
+#			# pragma once       #include "filename.h"
+#
+#			// code             // code
+#
+
+filenames="$@"
+
+echo "H Cpp - Generator"
+
+if [[ "$filenames" = "" ]]; then
+    echo "Empty argument!"
+    echo "Finished!"
+    exit 0
+fi
+
+for filenames
+do
+    echo "$filenames: $filenames.h $filenames.cpp"
+	printf "#pragma once\n\n// Code" > $filenames.h
+	printf "#include \"$filenames.h\"\n\n// Code" > $filenames.cpp
+done
+
+echo "Finished!"
