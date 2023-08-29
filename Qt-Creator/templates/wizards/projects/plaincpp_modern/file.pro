3ab37acbf68149d7acd8240b5459d9e4b5de16c4
Juan Pablo Enrici <jpenrici@gmail.com>
2023-08-29 20:53:48 -0300

    Update.

---
+++ Qt-Creator/templates/wizards/projects/plaincpp_modern/file.pro
@@ -0,0 +1,7 @@
+TEMPLATE = app
+CONFIG += console c++2b
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \\
+        %{CppFileName}
---
+++ Qt-Creator/templates/wizards/projects/plaincpp_modern/main.cpp
@@ -0,0 +1,9 @@
+%{Cpp:LicenseTemplate}\
+#include <iostream>
+
+auto main() -> int
+{
+    std::cout << "Modern C++ : Hello World!" << std::endl;
+
+    return 0;
+}
