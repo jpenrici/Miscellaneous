2e524835273c2f8d7a4c7308ed9261b5a460ec34
Juan Pablo Enrici <jpenrici@gmail.com>
2023-10-01 07:52:20 -0300

    Update.

--- Cpp/Simple_Header-Cpp_Generator/models/model_2.h
+++ Cpp/Simple_Header-Cpp_Generator/models/model_2.h
@@ -9,11 +9,10 @@ public:
     ModelClass() = default;
     ModelClass(const ModelClass &) = delete;
     ModelClass(ModelClass &&) = delete;
+    ~ModelClass() = default;
 
     auto operator=(const ModelClass &) -> ModelClass & = delete;
     auto operator=(ModelClass &&) -> ModelClass & = delete;
-
-    ~ModelClass() = default;
 
     auto getText() -> std::string;
     void setText(std::string newText);
