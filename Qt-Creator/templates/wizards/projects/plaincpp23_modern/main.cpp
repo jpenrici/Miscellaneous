6256d6591755c55a338f800b5771649e3fde780e
Juan Pablo Enrici <jpenrici@gmail.com>
2024-08-30 15:25:02 -0300

    Update.

--- Qt-Creator/templates/wizards/projects/plaincpp23_modern/main.cpp
+++ Qt-Creator/templates/wizards/projects/plaincpp23_modern/main.cpp
@@ -1,9 +1,23 @@
 %{Cpp:LicenseTemplate}\
+#include <iostream>
 #include <print>
 
 auto main() -> int
 {
-    std::println("Modern C++ 23 : Hello World!");
+    try {
+
+        // CODE
+        std::println("Modern C++ 23 : Hello World!");
+
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << '\\n';
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "Error: Unknown exception.\\n";
+        return 1;
+    }
 
     return 0;
 }
