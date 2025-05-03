c9c968c7cd25ab9500b7a97bc296fa08b50948cd
Juan Pablo Enrici <jpenrici@gmail.com>
2025-05-02 21:56:13 -0300

    Update.

--- Qt-Creator/templates/wizards/projects/plaincpp23_modern_cmake/main.cpp
+++ Qt-Creator/templates/wizards/projects/plaincpp23_modern_cmake/main.cpp
@@ -1,15 +1,7 @@
-#include "example.hpp"
-
 #include <print>
 
 auto main() -> int {
   std::println("Modern C++ 23 : Hello World!");
-
-  if (test()) {
-    std::println("Example is working!");
-  } else {
-    std::println("Example is not working!");
-  }
 
   return 0;
 }
