465bc0249d2744466f3cbf1218fa4b896d21e7ef
Juan Pablo Enrici <jpenrici@gmail.com>
2026-04-23 17:52:53 -0300

    list_packages.sh updated for Perl.

--- OpenBox/list_packages.pl
+++ OpenBox/list_packages.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-#
+# ########################################################
 # Simple script to help reinstall packages.
 # Run when it is necessary to remember installed packages.
 #
@@ -7,8 +7,11 @@
 #   1. list installed packages.
 #   2. analyze dependencies.
 #   3. generate and save recovery list.
-
-# Adapting for Perl!!! ...
+#
+# Use:
+#   perl list_packages.pl <directory/filename.txt>
+#   perl list_packages.pl --help
+# ########################################################
 
 use strict;
 use utf8;
@@ -20,54 +23,84 @@ use POSIX qw(strftime);
 
 sub main {
 
-    my @args   = @_;
-    my $script = "list_packages.pl";
+    print "Run list_packages.pl ...\n";
 
-    # Check for help or empty args
-    if ( !@args || $args[0] eq '--help' ) {
-        warn "Error: No argument.\n";
+    my $arg = $_[0] // "";
+
+    # Check for help or empty arg
+    if ( !$arg || $arg eq '--help' ) {
         show_help();
         return 1;
     }
 
     # Check path
-    my ( $filename, $directories ) = fileparse( $args[0] );
+    my ( $name, $directories, $extension ) = fileparse( $arg, qr/\.[^.]*/ );
 
     if ( !-d $directories ) {
-        warn "Error: Invalid path.\n";
+        warn "Error: Invalid path ('$directories').\n";
         show_help();
         return 1;
     }
 
-    if ( $filename !~ /\.txt$/i ) {
-        warn "Error: Invalid extension.\n";
+    if ( $extension !~ /\.txt$/i ) {
+        warn "Error: Invalid extension ('$extension').\n";
         show_help();
         return 1;
     }
 
-    # Run
-    print "Run $script ...\n";
+    my $today         = strftime( "%Y-%m-%d", localtime );
+    my $recovery_path = $directories . $name . "_" . $today . $extension;
 
-    my $today = strftime "%Y-%m-%d", localtime();
+    # 1. List of all Installed Packages
+    my @package_list;
+    my $installed_raw = list_packages_ref();
 
-    my $packageList    = "/tmp/packageList";
-    my $dependencyList = "/tmp/dependencyList";
-    my $recoveryList   = "recoveryList-$today";
+    # 2. Analyze dependencies
+    my %dependency_map;
+    my $counter = 1;
+    foreach my $line (@$installed_raw) {
+        chomp($line);
+        if ( $line =~ /^install ok installed:(.+)$/ ) {
+            my $package = $1;
+            push( @package_list, $package );
+            my $deps_raw = list_dependencies_ref($package);
 
-    # List of all Installed Packages
-    my $packages = list_packages_ref();
+            # Adds dependencies to the hash for faster searching.
+            foreach my $dep ( split( ' ', $deps_raw ) ) {
+                $dependency_map{$dep} = 1;
+            }
 
-    # Test
-    foreach my $p (@$packages) {
-        print "$p\n";
+            print("$counter : $package ...\n");
+            $counter++;
+        }
     }
+
+    # 3. Generate and save Recovery List
+    open( my $fh, '>', $recovery_path )
+      or die "The file could not be created.: $!";
+
+    print $fh "# $recovery_path\n";
+    print $fh "apt install \\\n";
+
+    # Filters packages that are NOT in the dependency list of others.
+    my @filtered_packages = sort grep { !$dependency_map{$_} } @package_list;
+
+    foreach my $pkg (@filtered_packages) {
+        print $fh "\t$pkg \\\n";
+    }
+
+    print $fh "; # $recovery_path\n";
+    close($fh);
+
+    print "\nDone! List saved in: $recovery_path\n";
 
     return 0;
 }
 
 sub list_packages_ref {
-    my $cmd    = "dpkg-query --show --showformat='\${Status;7}:\${Package} '";
-    my $result = qx{$cmd 2>/dev/null};
+
+    my $cmd    = "dpkg-query --show --showformat='\${Status}:\${Package}\\n'";
+    my @result = qx{$cmd 2>/dev/null};
 
     if ( $? != 0 ) {
         warn
@@ -75,13 +108,26 @@ sub list_packages_ref {
         return [];
     }
 
-    # if the result is null or contains only spaces, it returns an empty array.
-    return [] unless $result && $result =~ /\S/;
+    return \@result;
+}
 
-    # separate at any sequence of spaces and line breaks.
-    my @list = split( /\s+/, $result );
+sub list_dependencies_ref {
+    my $package = $_[0];
+    my $cmd     = "dpkg-query -f='\${Depends}' -W $package";
+    my $result  = qx{$cmd 2>/dev/null};
 
-    return \@list;
+    if ( $? != 0 ) {
+        warn
+          "Warning: The command executed, but returned an error (status $?).";
+        return "";
+    }
+
+    # Cleaning: removes versions enclosed in parentheses
+    #           (e.g., (>= 1.0)) and punctuation.
+    $result =~ s/\([^)]+\)//g;
+    $result =~ s/[,|]//g;
+
+    return \$result;
 }
 
 sub show_help() {
