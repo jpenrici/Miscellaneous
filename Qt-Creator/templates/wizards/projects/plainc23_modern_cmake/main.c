d05e4724bdf27aa401bc53816d3b17a8f2ece752
Juan Pablo Enrici <jpenrici@gmail.com>
2026-04-28 19:46:20 -0300

    Update.

--- Qt-Creator/templates/wizards/projects/plainc23_modern_cmake/main.c
+++ Qt-Creator/templates/wizards/projects/plainc23_modern_cmake/main.c
@@ -5,7 +5,7 @@ int main()
     auto message = "Hello, Modern C23 world!";
 
     if (message != nullptr) {
-        printf("%s\n", message);
+        printf("%s\\n", message);
     }
 
     return 0;
